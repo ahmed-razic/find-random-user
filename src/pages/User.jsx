1d9837009105b2c2eff02fe262e395e366aa620a
ahmed-razic <ahmed.razic@gmail.com>
2022-05-15 19:48:28 +0200

    user details page

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -13,66 +13,74 @@ function User() {
   }
 
   return (
-    <div className='w-full mx-auto lg:w-10/12'>
-      <div className='mb-4'>
-        <Link to='/' className='btn btn-ghost'>
+    <div className='container'>
+      <div className='flex justify-center mb-8'>
+        <Link to='/' className='btn mb-4 bg-indigo-700 border-none btn-wide'>
           Back To Home
         </Link>
       </div>
-      <div className='grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8'>
-        <div className='custom-card-image mb-6 md:mb-0'>
-          <div className='rounded-lg shadow-xl card image-full'>
-            <figure>
-              <img src={user[0].picture.large} alt='avatar' />
-            </figure>
-            <div className='card-body justify-end'>
-              <h2 className='card-title mb-0'>
-                {user[0].name.title} {user[0].name.first} {user[0].name.last}
-              </h2>
-              <h2 className='card-title mb-0'>{user[0].gender}</h2>
-              <h2 className='card-title mb-0'>Age {user[0].dob.age}</h2>
-              <p className='flex-grow-0'>{user[0].login.username}</p>
+      <div className='flex justify-center mb-8'>
+        <img src={user[0].picture.large} alt='avatar' />
+      </div>
+      <div className='flex justify-center mb-8'>
+        <h2 className='text-lg stat-value'>
+          {user[0].name.title} {user[0].name.first} {user[0].name.last}
+        </h2>
+      </div>
+      <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+        {user[0].location && (
+          <div className='stat'>
+            <div className='stat-title text-md'>Location</div>
+            <div className='text-lg stat-value'>
+              {user[0].location.city}, {user[0].location.state}
             </div>
           </div>
-        </div>
-
-        <div className='col-span-6'>
-          <div className='w-full rounded-lg shadow-md bg-base-100 stats'>
-            {user[0].location && (
-              <div className='stat'>
-                <div className='stat-title text-md'>Location</div>
-                <div className='text-lg stat-value'>
-                  {user[0].location.city}, {user[0].location.state}
-                </div>
-              </div>
-            )}
-            {user[0].phone && (
-              <div className='stat'>
-                <div className='stat-title text-md'>Phone</div>
-                <div className='text-lg stat-value'>{user[0].phone}</div>
-              </div>
-            )}
-            {user[0].cell && (
-              <div className='stat'>
-                <div className='stat-title text-md'>Cell</div>
-                <div className='text-lg stat-value'>{user[0].cell}</div>
-              </div>
-            )}
-            {user[0].email && (
-              <div className='stat'>
-                <div className='stat-title text-md'>Email</div>
-                <div className='text-lg stat-value'>{user[0].email}</div>
-              </div>
-            )}
-            {user[0].nat && (
-              <div className='stat'>
-                <div className='stat-title text-md'>Nationality</div>
-                <div className='text-lg stat-value'>{user[0].nat}</div>
-              </div>
-            )}
+        )}
+        {user[0].login.username && (
+          <div className='stat'>
+            <div className='stat-title text-md'>Username</div>
+            <div className='text-lg stat-value'>{user[0].login.username}</div>
           </div>
-        </div>
+        )}
+        {user[0].gender && (
+          <div className='stat'>
+            <div className='stat-title text-md'>Gender</div>
+            <div className='text-lg stat-value'>{user[0].gender}</div>
+          </div>
+        )}
+        {user[0].dob.age && (
+          <div className='stat'>
+            <div className='stat-title text-md'>Age</div>
+            <div className='text-lg stat-value'>{user[0].dob.age}</div>
+          </div>
+        )}
+        {user[0].phone && (
+          <div className='stat'>
+            <div className='stat-title text-md'>Phone</div>
+            <div className='text-lg stat-value'>{user[0].phone}</div>
+          </div>
+        )}
+        {user[0].cell && (
+          <div className='stat'>
+            <div className='stat-title text-md'>Cell</div>
+            <div className='text-lg stat-value'>{user[0].cell}</div>
+          </div>
+        )}
+        {user[0].email && (
+          <div className='stat'>
+            <div className='stat-title text-md'>Email</div>
+            <div className='text-lg stat-value'>{user[0].email}</div>
+          </div>
+        )}
+        {user[0].nat && (
+          <div className='stat'>
+            <div className='stat-title text-md'>Nationality</div>
+            <div className='text-lg stat-value'>{user[0].nat}</div>
+          </div>
+        )}
       </div>
+
+      <div className='col-span-6'></div>
     </div>
   )
 }
